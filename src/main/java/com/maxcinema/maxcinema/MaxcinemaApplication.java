7ef6f4a4d0aae1373d3ca2ddf55a77b98801ecf1
unknown <SSDD@POAPCA01LC0425.duoc.acad>
2026-05-06 18:21:10 -0400

    corrige gitignore

---
+++ src/main/java/com/maxcinema/maxcinema/MaxcinemaApplication.java
@@ -0,0 +1,13 @@
+package com.maxcinema.maxcinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxcinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxcinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxcinema/maxcinema/MaxcinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxcinema.maxcinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxcinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
