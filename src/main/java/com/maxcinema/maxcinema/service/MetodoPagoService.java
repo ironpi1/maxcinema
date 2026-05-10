14154323266de45f4c5c2235b027b91c46f57d47
ironpi1 <ro.perezt@duocuc.cl>
2026-05-10 17:32:17 -0400

    prueba para merge

--- src/main/java/com/maxcinema/maxcinema/service/MetodoPagoService.java
+++ src/main/java/com/maxcinema/maxcinema/service/MetodoPagoService.java
@@ -3,6 +3,8 @@ package com.maxcinema.maxcinema.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.maxcinema.maxcinema.DTO.MetodoPagoDTO;
+import com.maxcinema.maxcinema.model.MetodoPago;
 import com.maxcinema.maxcinema.repository.MetodoPagoRepository;
 
 @Service
@@ -10,5 +12,10 @@ public class MetodoPagoService {
     @Autowired
     private MetodoPagoRepository metodoPagoRepository;
 
+    public MetodoPagoDTO convertirADTO(MetodoPago metodoPago) {
+        MetodoPagoDTO dto = new MetodoPagoDTO();
+        dto.setTipo(metodoPago.getTipoPago());
+        return dto;
+    }
     
 }
--- src/main/java/com/maxcinema/maxcinema/service/TipoAsientoService.java
+++ src/main/java/com/maxcinema/maxcinema/service/TipoAsientoService.java
@@ -1,5 +1,19 @@
 package com.maxcinema.maxcinema.service;
 
-public class TipoAsientoService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.maxcinema.maxcinema.DTO.TipoAsientoDTO;
+import com.maxcinema.maxcinema.model.TipoAsiento;
+import com.maxcinema.maxcinema.repository.TipoAsientoRepository;
+
+@Service
+public class TipoAsientoService {
+    @Autowired
+    private TipoAsientoRepository tipoAsientoRepository;
+    public TipoAsientoDTO convertirADTO(TipoAsiento tipoAsiento) {
+        TipoAsientoDTO dto = new TipoAsientoDTO();
+        dto.setNombre(tipoAsiento.getNombre());
+        return dto;
+    }
 }
