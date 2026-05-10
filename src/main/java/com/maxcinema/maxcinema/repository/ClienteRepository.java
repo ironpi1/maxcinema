4ea1929e7ae30ee421ec1176848d4aae3a1f0737
ironpi1 <ro.perezt@duocuc.cl>
2026-05-09 23:50:40 -0400

    Finalizar Atributos del model

--- src/main/java/com/maxcinema/maxcinema/repository/ClienteRepository.java
+++ src/main/java/com/maxcinema/maxcinema/repository/ClienteRepository.java
@@ -1,5 +1,12 @@
 package com.maxcinema.maxcinema.repository;
 
-public interface ClienteRepository {
+import org.hibernate.validator.constraints.pl.REGON;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.maxcinema.maxcinema.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
     
 }
