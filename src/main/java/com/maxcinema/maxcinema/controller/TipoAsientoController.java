83d365bfe215d23c64154ab58dfb3e925bd98f27
ironpi1 <ro.perezt@duocuc.cl>
2026-05-10 23:06:12 -0400

    pequena correccion en TipoAsientoController

--- src/main/java/com/maxcinema/maxcinema/controller/TipoAsientoController.java
+++ src/main/java/com/maxcinema/maxcinema/controller/TipoAsientoController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,11 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.maxcinema.maxcinema.DTO.MetodoPagoDTO;
 import com.maxcinema.maxcinema.DTO.TipoAsientoDTO;
-import com.maxcinema.maxcinema.model.MetodoPago;
 import com.maxcinema.maxcinema.model.TipoAsiento;
-import com.maxcinema.maxcinema.repository.TipoAsientoRepository;
 import com.maxcinema.maxcinema.service.TipoAsientoService;
 
 import lombok.extern.slf4j.Slf4j;
