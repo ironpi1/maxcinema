5af289d5cec2b1cf630e1460e412ba1a550016f1
miyamotomusashi55 <ignaci.fuentesc@duocuc.cl>
2026-06-06 19:38:46 -0400

    se agregan los microservicios en la carpeta (Sistema-maxcinema)

---
+++ Sistema-maxcinema/salas/src/test/java/com/sala/salas/SalasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sala.salas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
