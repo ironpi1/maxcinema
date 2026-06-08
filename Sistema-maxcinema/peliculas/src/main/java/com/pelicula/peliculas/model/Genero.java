f87a6e3291f0cb33345661f791c77931ce79eaf5
antoaliaga <a.aliaga@duocuc.cl>
2026-06-08 18:11:47 -0400

    feat: agregar modelos del microservicio peliculas

---
+++ Sistema-maxcinema/peliculas/src/main/java/com/pelicula/peliculas/model/Genero.java
@@ -0,0 +1,37 @@
+package com.pelicula.peliculas.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@AllArgsConstructor
+@Table(name = "genero")
+public class Genero {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "El nombre del genero no puede estar vacio")
+    @Size(min = 10, max = 50, message = "El nombre de la pelicula debe tener entre 4 y 50 caracteres")
+    @Column(nullable = false, length = 50)
+    private String nombre;
+
+    // Relación hacia la tabla puente
+    @OneToMany(mappedBy = "genero")
+    private List<Generos> peliculas;
+}
