fbc0260ad7964bdc6ee2ee540b1e782fb6ce83db
miyamotomusashi55 <ignaci.fuentesc@duocuc.cl>
2026-06-12 17:07:28 -0400

    feat: se terminan de crear las tablas de películas y se crea

--- Sistema-maxcinema/peliculas/src/main/resources/db/migration/V1_crear_tablas_peliculas.sql
+++ Sistema-maxcinema/peliculas/src/main/resources/db/migration/V1_crear_tablas_peliculas.sql
@@ -1,5 +1,4 @@
 CREATE TABLE pelicula (
-
     id INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR2(50) NOT NULL,
     descripcion VARCHAR2(250) NOT NULL,
@@ -9,13 +8,11 @@ CREATE TABLE pelicula (
 );
 
 CREATE TABLE genero (
-
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR2(50) NOT NULL
 );
 
 CREATE TABLE generos (
-
     id INT AUTO_INCREMENT PRIMARY KEY,
     pelicula_id INT NOT NULL,
     genero_id INT NOT NULL,
@@ -24,17 +21,35 @@ CREATE TABLE generos (
 );
 
 CREATE TABLE director (
-
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR2(50) NOT NULL,
     nacionalidad VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE directores (
-
     id INT AUTO_INCREMENT PRIMARY KEY,
     director_id INT NOT NULL,
     pelicula_id INT NOT NULL,
     CONSTRAINT fk_directores_director FOREIGN KEY (director_id) REFERENCES director(id),
     CONSTRAINT fk_directores_pelicula FOREIGN KEY (pelicula_id) REFERENCES pelicula(id),
 );
+
+CREATE TABLE idioma (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR2(20) NOT NULL
+);
+
+CREATE TABLE idiomas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pelicula_id INT NOT NULL,
+    idioma_id INT NOT NULL,
+    CONSTRAINT fk_idiomas_pelicula FOREIGN KEY (pelicula_id) REFERENCES pelicula(id),
+    CONSTRAINT fk_idiomas_idioma FOREIGN KEY (idioma_id) REFERENCES idioma(id)
+);
+
+CREATE TABLE salasPelicula (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pelicula_id INT NOT NULL,
+    sala_id INT NOT NULL,
+    CONSTRAINT fk_salaspelicula_pelicula FOREIGN KEY (pelicula_id) REFERENCES pelicula(id)
+)
