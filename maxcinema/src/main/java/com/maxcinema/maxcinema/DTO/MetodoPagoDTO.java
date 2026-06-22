3c95a5f4a1a93566fac8452971b694af896102e8
antoaliaga <a.aliaga@duocuc.cl>
2026-06-22 18:12:42 -0400

    eliminacion de maxcinema antiguo
    se borra el sistema antiguo de maxcinema y se corrigen errores menores

--- maxcinema/src/main/java/com/maxcinema/maxcinema/DTO/MetodoPagoDTO.java
+++
@@ -1,9 +0,0 @@
-package com.maxcinema.maxcinema.DTO;
-
-import lombok.Data;
-
-@Data
-public class MetodoPagoDTO {
-    private Integer id;
-    private String nombreTipo;
-}
--- maxcinema/src/main/java/com/maxcinema/maxcinema/repository/ComunaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.maxcinema.maxcinema.repository;
-import com.maxcinema.maxcinema.model.Comuna;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-import java.util.List;
-public interface ComunaRepository extends JpaRepository <Comuna, Integer> {
-    List<Comuna> findByNombre(String nombre);
-}
