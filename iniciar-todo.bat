995e577805cb099ff6eb532b697b27157d3ac038
miyamotomusashi55 <ignaci.fuentesc@duocuc.cl>
2026-06-26 17:47:35 -0400

    fix: se arregló archivo .bat

--- iniciar-todo.bat
+++ iniciar-todo.bat
@@ -1,60 +1,32 @@
+
 @echo off
-
  
-
 echo Iniciando Servidor de Descubrimiento Eureka (Puerto 8761)...
-
-cd eureka
-
-start cmd /k "mvnw spring-boot:run"
-
+cd Sistema-maxcinema\eureka
+start cmd /k "mvnw.cmd spring-boot:run"
  
-
-echo Esperando 12 segundos a que Eureka se estabilice...
-
-timeout /t 12 /nobreak > null
-
+echo Esperando 15 segundos a que Eureka se estabilice...
+timeout /t 15 /nobreak > nul
  
-
 echo Iniciando API Gateway...
-
-cd ../gateway
-
-start cmd /k "mvnw spring-boot:run"
-
+cd ..\gateway
+start cmd /k "mvnw.cmd spring-boot:run"
  
-
 echo Iniciando Microservicio Peliculas...
-
-cd ../peliculas
-
-start cmd /k "mvnw spring-boot:run"
-
+cd ..\peliculas
+start cmd /k "mvnw.cmd spring-boot:run"
  
-
 echo Iniciando Microservicio Cines...
-
-cd ../cines
-
-start cmd /k "mvnw spring-boot:run"
-
+cd ..\cines
+start cmd /k "mvnw.cmd spring-boot:run"
  
-
 echo Iniciando Microservicio Salas...
-
-cd ../salas
-
-start cmd /k "mvnw spring-boot:run"
-
+cd ..\salas
+start cmd /k "mvnw.cmd spring-boot:run"
  
-
 echo Iniciando Microservicio Clientes...
-
-cd ../clientes
-
-start cmd /k "mvnw spring-boot:run"
-
+cd ..\clientes
+start cmd /k "mvnw.cmd spring-boot:run"
  
-
+echo.
 echo Ecosistema MaxCinema lanzado. Dashboard disponible en http://localhost:8761
-
